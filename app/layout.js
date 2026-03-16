664846ca7b2901ecbe9b2132c178f2e241bcf72f
domg6517 <domg6517@gmail.com>
2026-03-15 17:14:30 -0700

    Fix: remove old SW registration, keep only unregister to fix iOS audio

--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,6 @@ if ('serviceWorker' in navigator) {
             navigator.serviceWorker.getRegistrations().then(function(regs) {
               regs.forEach(function(r) { r.unregister(); });
             });
-          }if ('serviceWorker' in navigator) {
-            window.addEventListener('load', function() {
-              navigator.serviceWorker.register('/sw.js').then(function(reg) {
-                reg.addEventListener('updatefound', function() {
-                  var newWorker = reg.installing;
-                  newWorker.addEventListener('statechange', function() {
-                    if (newWorker.state === 'activated') {
-                      window.location.reload();
-                    }
-                  });
-                });
-              });
-            });
           }
         `}} />
       </body>
